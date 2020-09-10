9368b4894f4103b999b784e4df29206d9a53f14a
muzashi777 <muzashi777@gmail.com>
2020-09-10 07:28:22 +0700

    fullpage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./index.css";
 import Index from "./components/index";
 import Nav from "./components/nav";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Route path="/" exact component={Index} />
-//     </Router>
-//   );
-// }
 
 function App() {
   return (
